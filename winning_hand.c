3627f74fe87e7a19c82d9307e152c4a492a92899
JimmyLuo <jluo00@stuy.edu>
2020-01-20 20:30:34 -0500

    idk

--- winning_hand.c
+++ winning_hand.c
@@ -15,27 +15,27 @@ two pair: 6
 one pair: 7
 high card: 8
 */
-// 
-// int hand_type(struct card * hand, struct flop * flop){
-//   if(flush(hand,flop,flopsize)){
-//     // if(straight(hand,flop,flopsize))
-//     //   return 0;
-//     // else
-//      return 3;
-//   }
-//   // if(two_pair(hand,flop,flopsize))
-//   //   return 6;
-//   // if(pair(hand,flop,flopsize))
-//   //   return 7;
-//   // if(straight(hand,flop,flopsize))
-//   //   return 4;
-//   // if(triple(hand,flop,flopsize)){
-//   //   if(house(hand,flop,flopsize))
-//   //     return 2;
-//   //   else
-//   //     return 5;
-//   // }
-//   // if(quad(hand,flop,flopsize))
-//   //   return 1;
-//   return 8;
-// }
+
+int hand_type(struct hand * hand, struct flop * flop){
+  //if(flush){
+    // if(straight)
+    //   return 0;
+    // else
+     //return 3;
+  //}
+  // if(two_pair)
+  //   return 6;
+  // if(pair)
+  //   return 7;
+  // if(straight)
+  //   return 4;
+  // if(triple){
+  //   if(house)
+  //     return 2;
+  //   else
+  //     return 5;
+  // }
+  // if(quad)
+  //   return 1;
+  return 8;
+}
--- winning_hand.h
+++ winning_hand.h
@@ -1,1 +1,2 @@
-
+int hand_type(struct hand * hand, struct flop * flop);
+int flush(struct hand * hand, struct flop * flop);
